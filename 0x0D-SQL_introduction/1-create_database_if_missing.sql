91217f81aaa4cba48e092b13b30dd0a8f57819b6
oussama <oussama@KALI.168.1.10>
2024-02-16 01:18:57 +0100

    1

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,3 @@
---create a database hbtn_0c_0 im my MySQL service
+--create a database hbtn_0c_0 im my MySQL servic 
+--If database already exists, the script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
