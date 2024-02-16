c4a4255420602e2fcdc0e2c41b135235073ecf21
oussama <oussama@KALI.168.1.10>
2024-02-16 15:39:21 +0100

    1

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,2 @@
---create a database 'hbtn_0c_0' im my MySQL servic 
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
--- if 
-
+-- Creates the database hbtn_0c_0 in my MySQL server.
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
