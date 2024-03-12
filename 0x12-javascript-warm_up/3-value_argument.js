b87fe4332740658bbc4675af5099e7d52efd89b3
oussama <oussama@KALI.168.1.10>
2024-03-12 00:01:59 +0000

    3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-let A = [];
-A = process.argv;
-if (A[2]) {
-  console.log(A[2]);
-} else if (!A[2]) { console.log('No argument'); }
+if (process.argv[2] === undefined) {
+  console.log('No argument');
+} else { console.log(process.argv[2]); }
