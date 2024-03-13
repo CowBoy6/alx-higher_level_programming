5166ccac149bcd1ab7df3b443d37071c419d6131
oussama <oussama@KALI.168.1.10>
2024-03-13 02:40:56 +0000

    8

---
+++ 0x12-javascript-warm_up/h.js
@@ -0,0 +1,6 @@
+console.log('He said, "Don\'t do that!"'); // Using \' to include a single quote inside a single-quoted string
+console.log("She said, \"I'm coming!\""); // Using \" to include double quotes inside a double-quoted string
+console.log("Backspace\bTest"); // Using \b to move the cursor one position to the left
+console.log("Carriage\rReturn"); // Using \r to return the cursor to the beginning of the line
+console.log("Form\fFeed"); // Using \f for page-breaking or clearing the screen
+console.log("Vertical\vTab"); // Using \v for vertical spacing or formatting
