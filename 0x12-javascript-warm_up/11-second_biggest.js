4d89b528aa38e546d9057b98f8f285658595dc2d
oussama <oussama@KALI.168.1.10>
2024-03-13 17:52:53 +0000

    11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/bin/bash
+if (process.argv[2] === 0 || process.argv[2] === 1) { console.log('1'); } else {
+  let A = [];
+  let k = 0;
+  for (let i = 2; i < process.argv.length; i++) {
+    A[k] = process.argv[i];
+    k++;
+  }
+  A = A.sort(function (a, b) { return b - a; });
+  console.log(A[1]);
+}
