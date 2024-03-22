318a140442850ea28d2dd093448711c89f354ef4
oussama <oussama@KALI.168.1.10>
2024-03-22 03:28:58 +0000

    2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Write a script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usawhere name matches the argument"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    """ main function"""
+    db = MySQLdb.connect(host="localhost", passwd=sys.argv[2],
+                         user=sys.argv[1], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s", (sys.argv[4], ))
+    data = cur.fetchall()
+    for row in data:
+        print(row)
+    cur.close()
+    db.close()
